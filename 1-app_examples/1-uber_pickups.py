432dfa4f5a277c7389a1a1c0e4490f7cfeee99be
adammln <gitarbetot@gmail.com>
2022-06-02 12:27:33 +0700

    docs: add reference

--- 1-app_examples/1-uber_pickups.py
+++ 1-app_examples/1-uber_pickups.py
@@ -5,6 +5,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
+st.write("*reference: https://docs.streamlit.io/library/get-started/create-an-app*")
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
